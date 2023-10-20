ae80d3b0e940acb6dbb247fd1a9c23d6fb75ba13
Joikil <nikhilssteja@gmail.com>
2023-10-20 12:23:49 +0530

    now we can continue game where we left off

--- ttt_play.py
+++ ttt_play.py
@@ -1,9 +1,12 @@
 from ttt_game import TicTacToeState,Action,Record,Sign,Turn
+import os.path
 
 class TicTacToe:
     def __init__(self):
         self.state = TicTacToeState()
-        
+        self.record= Record()
+        self.f_name="Game_record.txt"
+
     def play(self):
         while not self.state.ended():
             self.display_board()
@@ -16,6 +19,7 @@ class TicTacToe:
             print(f"Player {winner} wins!")
         else:
             print("It's a draw!")
+        os.remove(self.f_name)
     def get_player_move(self):
         while True:
             try:
@@ -27,7 +31,14 @@ class TicTacToe:
             except (ValueError, IndexError):
                 print("Invalid input. Please enter two numbers (0-2) separated by a space.")
 
-    def display_board(self): 
+    def display_board(self):
+        if self.state.board == [[Sign._ for _ in range(3)] for _ in range(3)] and os.path.isfile(self.f_name) and os.stat(self.f_name)!=0:
+                a=input("To restore previos session enter 'r' or Any key to start a new game:")
+                if a == 'r':
+                    l=self.record.action_to_g_board(self.f_name)
+                    for i in l:
+                        a=Action(i)
+                        self.state.dispatch(a) 
         for row in self.state.board:
             print(" ".join(str(sign.name) for sign in row))
             print()
